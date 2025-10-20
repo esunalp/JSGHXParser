ac051722eb35dd2a13928f86ddbe025c5b872dda
esunalp <92453869+esunalp@users.noreply.github.com>
2025-10-20 17:49:41 +0200

    Update THREE.js import to use CDN version

--- poc-ghx-three/three-scene.js
+++ poc-ghx-three/three-scene.js
@@ -1,4 +1,4 @@
-import * as THREE from './vendor/three.module.js?version=3';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.180.0/build/three.module.js';
 import { OrbitControls } from './vendor/OrbitControls.js?version=3';
 
 function createRenderer(canvas) {
