1cc78e085bc8cab94487f62bb7cb48c827f9c720
esunalp <92453869+esunalp@users.noreply.github.com>
2025-10-20 18:27:12 +0200

    feat: add complex component metadata to GHX parser

---
+++ poc-ghx-three/component-metadata.js
@@ -0,0 +1,464 @@
+export const COMPLEX_COMPONENTS = [
+  {
+    "category": "Complex",
+    "subcategory": "Polynomials",
+    "name": "Square",
+    "nickname": "Sqr",
+    "guid": "0b0f1203-2ea8-4250-a45a-cca7ad2e5b76",
+    "description": "Compute the square of a complex number.",
+    "inputs": [
+      {
+        "name": "Input",
+        "nickname": "x",
+        "access": "item",
+        "description": "Input value"
+      }
+    ],
+    "outputs": [
+      {
+        "name": "Output",
+        "nickname": "y",
+        "access": "item",
+        "description": "Output value"
+      }
+    ]
+  },
+  {
+    "category": "Complex",
+    "subcategory": "Trig",
+    "name": "Tangent",
+    "nickname": "Tan",
+    "guid": "0bc93049-e1a7-44b5-8068-c7ddc85a9f46",
+    "description": "Compute the tangent of a complex angle.",
+    "inputs": [
+      {
+        "name": "Input",
+        "nickname": "x",
+        "access": "item",
+        "description": "Input value"
+      }
+    ],
+    "outputs": [
+      {
+        "name": "Output",
+        "nickname": "y",
+        "access": "item",
+        "description": "Output value"
+      }
+    ]
+  },
+  {
+    "category": "Complex",
+    "subcategory": "Polynomials",
+    "name": "Power",
+    "nickname": "Pow",
+    "guid": "2d6cb24f-da89-4fab-be0f-e5d439e0217a",
+    "description": "Raise a complex number to a complex power.",
+    "inputs": [
+      {
+        "name": "First number",
+        "nickname": "A",
+        "access": "item",
+        "description": "First number"
+      },
+      {
+        "name": "Second number",
+        "nickname": "B",
+        "access": "item",
+        "description": "Second number"
+      }
+    ],
+    "outputs": [
+      {
+        "name": "Result",
+        "nickname": "R",
+        "access": "item",
+        "description": "Result"
+      }
+    ]
+  },
+  {
+    "category": "Complex",
+    "subcategory": "Operators",
+    "name": "Multiplication",
+    "nickname": "Multiply",
+    "guid": "2f643ab6-b9a4-4923-b3da-f9d52b0cba14",
+    "description": "Multiply two complex numbers.",
+    "inputs": [
+      {
+        "name": "First number",
+        "nickname": "A",
+        "access": "item",
+        "description": "First number"
+      },
+      {
+        "name": "Second number",
+        "nickname": "B",
+        "access": "item",
+        "description": "Second number"
+      }
+    ],
+    "outputs": [
+      {
+        "name": "Result",
+        "nickname": "R",
+        "access": "item",
+        "description": "Result"
+      }
+    ]
+  },
+  {
+    "category": "Complex",
+    "subcategory": "Trig",
+    "name": "CoTangent",
+    "nickname": "Cotan",
+    "guid": "39461433-ac44-4298-94a9-988f983e347c",
+    "description": "Compute the co-tangent of a complex angle.",
+    "inputs": [
+      {
+        "name": "Input",
+        "nickname": "x",
+        "access": "item",
+        "description": "Input value"
+      }
+    ],
+    "outputs": [
+      {
+        "name": "Output",
+        "nickname": "y",
+        "access": "item",
+        "description": "Output value"
+      }
+    ]
+  },
+  {
+    "category": "Complex",
+    "subcategory": "Trig",
+    "name": "ArcTangent",
+    "nickname": "ATan",
+    "guid": "4e8aad42-9111-470c-9acd-7ae365d8bba4",
+    "description": "Compute the arc-tangent of a complex angle.",
+    "inputs": [
+      {
+        "name": "Input",
+        "nickname": "x",
+        "access": "item",
+        "description": "Input value"
+      }
+    ],
+    "outputs": [
+      {
+        "name": "Output",
+        "nickname": "y",
+        "access": "item",
+        "description": "Output value"
+      }
+    ]
+  },
+  {
+    "category": "Complex",
+    "subcategory": "Polynomials",
+    "name": "Exponential",
+    "nickname": "Exp",
+    "guid": "582f96c6-ed0c-4710-9b5e-a05addba9f42",
+    "description": "Compute the exponential of a complex number.",
+    "inputs": [
+      {
+        "name": "Input",
+        "nickname": "x",
+        "access": "item",
+        "description": "Input value"
+      }
+    ],
+    "outputs": [
+      {
+        "name": "Output",
+        "nickname": "y",
+        "access": "item",
+        "description": "Output value"
+      }
+    ]
+  },
+  {
+    "category": "Complex",
+    "subcategory": "Operators",
+    "name": "Addition",
+    "nickname": "Add",
+    "guid": "58669268-a825-4688-8072-7d3508fcf91c",
+    "description": "Add two complex numbers.",
+    "inputs": [
+      {
+        "name": "First number",
+        "nickname": "A",
+        "access": "item",
+        "description": "First number"
+      },
+      {
+        "name": "Second number",
+        "nickname": "B",
+        "access": "item",
+        "description": "Second number"
+      }
+    ],
+    "outputs": [
+      {
+        "name": "Result",
+        "nickname": "R",
+        "access": "item",
+        "description": "Result"
+      }
+    ]
+  },
+  {
+    "category": "Complex",
+    "subcategory": "Polynomials",
+    "name": "Square Root",
+    "nickname": "Sqrt",
+    "guid": "5a22dc1a-907c-4e2f-b8da-0e496c4e25bb",
+    "description": "Compute the square-root of a complex number.",
+    "inputs": [
+      {
+        "name": "Input",
+        "nickname": "x",
+        "access": "item",
+        "description": "Input value"
+      }
+    ],
+    "outputs": [
+      {
+        "name": "Output",
+        "nickname": "y",
+        "access": "item",
+        "description": "Output value"
+      }
+    ]
+  },
+  {
+    "category": "Complex",
+    "subcategory": "Trig",
+    "name": "Cosine",
+    "nickname": "Cos",
+    "guid": "7874f26c-6f76-4da8-b527-2d567184b2bd",
+    "description": "Compute the cosine of a complex angle.",
+    "inputs": [
+      {
+        "name": "Input",
+        "nickname": "x",
+        "access": "item",
+        "description": "Input value"
+      }
+    ],
+    "outputs": [
+      {
+        "name": "Output",
+        "nickname": "y",
+        "access": "item",
+        "description": "Output value"
+      }
+    ]
+  },
+  {
+    "category": "Complex",
+    "subcategory": "Trig",
+    "name": "ArcCosine",
+    "nickname": "ACos",
+    "guid": "8640c519-9bf6-4e9a-a108-75f9d89b2c58",
+    "description": "Compute the arc-cosine of a complex angle.",
+    "inputs": [
+      {
+        "name": "Input",
+        "nickname": "x",
+        "access": "item",
+        "description": "Input value"
+      }
+    ],
+    "outputs": [
+      {
+        "name": "Output",
+        "nickname": "y",
+        "access": "item",
+        "description": "Output value"
+      }
+    ]
+  },
+  {
+    "category": "Complex",
+    "subcategory": "Trig",
+    "name": "Cosecant",
+    "nickname": "Cosec",
+    "guid": "99197a17-d5c7-419b-acde-eca2737f3c58",
+    "description": "Compute the co-secant of a complex angle.",
+    "inputs": [
+      {
+        "name": "Input",
+        "nickname": "x",
+        "access": "item",
+        "description": "Input value"
+      }
+    ],
+    "outputs": [
+      {
+        "name": "Output",
+        "nickname": "y",
+        "access": "item",
+        "description": "Output value"
+      }
+    ]
+  },
+  {
+    "category": "Complex",
+    "subcategory": "Operators",
+    "name": "Subtraction",
+    "nickname": "Minus",
+    "guid": "babecca6-9813-4146-b150-cd72f743e47c",
+    "description": "Subtract two complex numbers.",
+    "inputs": [
+      {
+        "name": "First number",
+        "nickname": "A",
+        "access": "item",
+        "description": "First number"
+      },
+      {
+        "name": "Second number",
+        "nickname": "B",
+        "access": "item",
+        "description": "Second number"
+      }
+    ],
+    "outputs": [
+      {
+        "name": "Result",
+        "nickname": "R",
+        "access": "item",
+        "description": "Result"
+      }
+    ]
+  },
+  {
+    "category": "Complex",
+    "subcategory": "Polynomials",
+    "name": "Logarithm",
+    "nickname": "Ln",
+    "guid": "bc4a27fc-cbb9-4802-bd4a-17ab33ad1826",
+    "description": "Compute the natural logarithm of a complex number.",
+    "inputs": [
+      {
+        "name": "Input",
+        "nickname": "x",
+        "access": "item",
+        "description": "Input value"
+      }
+    ],
+    "outputs": [
+      {
+        "name": "Output",
+        "nickname": "y",
+        "access": "item",
+        "description": "Output value"
+      }
+    ]
+  },
+  {
+    "category": "Complex",
+    "subcategory": "Trig",
+    "name": "Sine",
+    "nickname": "Sin",
+    "guid": "c53932eb-7c8c-4825-ae98-e36bba97232d",
+    "description": "Compute the sine of a complex angle.",
+    "inputs": [
+      {
+        "name": "Input",
+        "nickname": "x",
+        "access": "item",
+        "description": "Input value"
+      }
+    ],
+    "outputs": [
+      {
+        "name": "Output",
+        "nickname": "y",
+        "access": "item",
+        "description": "Output value"
+      }
+    ]
+  },
+  {
+    "category": "Complex",
+    "subcategory": "Operators",
+    "name": "Division",
+    "nickname": "Divide",
+    "guid": "cb4ec4a1-f48e-4685-b58c-72ed27b53681",
+    "description": "Divide two complex numbers.",
+    "inputs": [
+      {
+        "name": "First number",
+        "nickname": "A",
+        "access": "item",
+        "description": "First number"
+      },
+      {
+        "name": "Second number",
+        "nickname": "B",
+        "access": "item",
+        "description": "Second number"
+      }
+    ],
+    "outputs": [
+      {
+        "name": "Result",
+        "nickname": "R",
+        "access": "item",
+        "description": "Result"
+      }
+    ]
+  },
+  {
+    "category": "Complex",
+    "subcategory": "Trig",
+    "name": "Secant",
+    "nickname": "Sec",
+    "guid": "d879e74c-6fe3-4cbf-b3fa-60a7c48b73e7",
+    "description": "Compute the secant of a complex angle.",
+    "inputs": [
+      {
+        "name": "Input",
+        "nickname": "x",
+        "access": "item",
+        "description": "Input value"
+      }
+    ],
+    "outputs": [
+      {
+        "name": "Output",
+        "nickname": "y",
+        "access": "item",
+        "description": "Output value"
+      }
+    ]
+  },
+  {
+    "category": "Complex",
+    "subcategory": "Trig",
+    "name": "ArcSine",
+    "nickname": "ASin",
+    "guid": "f18091e9-3264-4dd4-9ba6-32c77fca0ac0",
+    "description": "Compute the arc-sine of a complex angle.",
+    "inputs": [
+      {
+        "name": "Input",
+        "nickname": "x",
+        "access": "item",
+        "description": "Input value"
+      }
+    ],
+    "outputs": [
+      {
+        "name": "Output",
+        "nickname": "y",
+        "access": "item",
+        "description": "Output value"
+      }
+    ]
+  }
+];
