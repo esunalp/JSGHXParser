d6090d4e9aa06bf60db07e12c78d3b42e5eb9140
esunalp <92453869+esunalp@users.noreply.github.com>
2025-10-23 10:36:41 +0200

    Adjust viewport helpers for millimeter scale

--- poc-ghx-three/three-scene.js
+++ poc-ghx-three/three-scene.js
@@ -1,6 +1,11 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
+const GRID_CELL_SIZE_MM = 1000;
+const GRID_DIVISIONS = 20;
+const GRID_SIZE_MM = GRID_CELL_SIZE_MM * GRID_DIVISIONS;
+const AXES_LENGTH_MM = GRID_CELL_SIZE_MM * 5;
+
 function createRenderer(canvas) {
   const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
@@ -20,11 +25,11 @@ function addDefaultLights(scene) {
 }
 
 function addHelpers(scene) {
-  const grid = new THREE.GridHelper(20, 20, 0x888888, 0x444444);
-  grid.position.y = -0.5;
+  const grid = new THREE.GridHelper(GRID_SIZE_MM, GRID_DIVISIONS, 0x888888, 0x444444);
+  grid.position.y = -0.5 * GRID_CELL_SIZE_MM;
   scene.add(grid);
 
-  const axes = new THREE.AxesHelper(5);
+  const axes = new THREE.AxesHelper(AXES_LENGTH_MM);
   scene.add(axes);
 }
 
