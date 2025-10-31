ae97bb2549ed9b218a76f5328251457117a37268
esunalp <92453869+esunalp@users.noreply.github.com>
2025-10-31 22:48:52 +0100

    Update three_integration.js

--- alpha/web/three_integration.js
+++ alpha/web/three_integration.js
@@ -15,6 +15,13 @@ import {
 } from 'three/tsl';
 import { ssr } from 'three/addons/tsl/display/SSRNode.js';
 import { smaa } from 'three/addons/tsl/display/SMAANode.js';
+import { PhysicalSunSky } from './physical-sun-sky.js';
+import {
+  cloneSurfaceMaterial,
+  createStandardSurfaceMaterial,
+  convertMaterialToNode,
+  ensureGeometryHasVertexNormals,
+} from './material-utils.js';
 
 THREE.Object3D.DEFAULT_UP.set(0, 0, 1);
 
